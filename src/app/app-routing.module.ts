8c70db97542ebcfd31f95bf208406e23c437d398
Srikee <kee19384@gmail.com>
2021-03-27 14:33:47 +0700

    เริ่มต้นแอพ ionic

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },  {
+    path: 'prayut',
+    loadChildren: () => import('./prayut/prayut.module').then( m => m.PrayutPageModule)
+  },
+  {
+    path: 'pravit',
+    loadChildren: () => import('./pravit/pravit.module').then( m => m.PravitPageModule)
+  },
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
